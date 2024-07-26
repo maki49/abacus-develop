f04432131665c571e018d4e187e127055539a53a
maki49 <1579492865@qq.com>
2024-07-26 16:47:33 +0800

    write T, VL, VNL, Hartree orbital energy separately

--- source/module_io/test/read_input_ptest.cpp
+++ source/module_io/test/read_input_ptest.cpp
@@ -197,6 +197,7 @@ TEST_F(InputParaTest, ParaRead)
     EXPECT_EQ(param.inp.out_mat_hs[1], 8);
     EXPECT_EQ(param.inp.out_mat_hs2, 0);
     EXPECT_EQ(param.inp.out_mat_xc, 0);
+    EXPECT_EQ(param.inp.out_eband_separate_term, 0);
     EXPECT_EQ(param.inp.out_interval, 1);
     EXPECT_EQ(param.inp.out_app_flag, 0);
     EXPECT_EQ(param.inp.out_mat_r, 0);
