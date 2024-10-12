0fbcaac9d154a3db792027962d9ed35ce70fd938
maki49 <1579492865@qq.com>
2024-10-12 20:21:22 +0800

    spin-up/down nocc, nvirt, npairs

--- source/module_lr/esolver_lrtd_lcao.h
+++ source/module_lr/esolver_lrtd_lcao.h
@@ -68,17 +68,20 @@ namespace LR
         /// @brief Excited state info. size: nstates * nks * (nocc(local) * nvirt (local))
         std::vector<std::shared_ptr<psi::Psi<T>>> X;
 
-        int nocc = 1;
+        std::vector<int> nocc = { 1, 1 };   ///< number of occupied orbitals for each spin used in the calculation
+        int nocc_in = 1;    ///< nocc read from input (adjusted by nelec): max(spin-up, spindown)
         int nocc_max = 1;   ///< nelec/2
-        int nvirt = 1;
+        std::vector<int> nvirt = { 1, 1 };   ///< number of virtual orbitals for each spin used in the calculation
+        int nvirt_in = 1;   ///< nvirt read from input (adjusted by nelec): min(spin-up, spindown)
         int nbands = 2;
         int nbasis = 2;
         /// n_occ*nvirt, the basis size of electron-hole pair representation
-        int npairs = 1;
+        std::vector<int> npairs = { 1, 1 };
         /// how many 2-particle states to be solved
         int nstates = 1;
         int nspin = 1;
         int nk = 1;
+        int nupdown = 0;
         std::string xc_kernel;
 
         Grid_Technique gt_;
@@ -90,7 +93,7 @@ namespace LR
         /// @brief variables for parallel distribution of KS orbitals
         Parallel_2D paraC_;
         /// @brief variables for parallel distribution of excited states
-        Parallel_2D paraX_;
+        std::vector<Parallel_2D> paraX_;
         /// @brief variables for parallel distribution of matrix in AO representation
         Parallel_Orbitals paraMat_;
 
@@ -111,6 +114,8 @@ namespace LR
         void parameter_check() const;
         /// @brief set nocc, nvirt, nbasis, npairs and nstates
         void set_dimension();
+        /// reset nocc, nvirt, npairs after read ground-state wavefunction when nspin=2
+        void reset_dim_spin2();
 
 #ifdef __EXX
         /// Tdata of Exx_LRI is same as T, for the reason, see operator_lr_exx.h
--- source/module_lr/hamilt_casida.cpp
+++ source/module_lr/hamilt_casida.cpp
@@ -5,21 +5,24 @@ namespace LR
     std::vector<T> HamiltCasidaLR<T>::matrix()
     {
         ModuleBase::TITLE("HamiltCasidaLR", "matrix");
-        int npairs = this->nocc * this->nvirt;
+        const int no = this->nocc[0];
+        const int nv = this->nvirt[0];
+        const auto& px = this->pX[0];
+        int npairs = no * nv;
         std::vector<T> Amat_full(this->nk * npairs * this->nk * npairs, 0.0);
         for (int ik = 0;ik < this->nk;++ik)
-            for (int j = 0;j < nocc;++j)
-                for (int b = 0;b < nvirt;++b)
+            for (int j = 0;j < no;++j)
+                for (int b = 0;b < nv;++b)
                 {//calculate A^{ai} for each bj
-                    int bj = j * nvirt + b;
+                    int bj = j * nv + b;
                     int kbj = ik * npairs + bj;
-                    psi::Psi<T> X_bj(1, 1, this->nk * this->pX->get_local_size()); // k1-first, like in iterative solver
+                    psi::Psi<T> X_bj(1, 1, this->nk * px.get_local_size()); // k1-first, like in iterative solver
                     X_bj.zero_out();
-                    // X_bj(0, 0, lj * this->pX->get_row_size() + lb) = this->one();
-                    int lj = this->pX->global2local_col(j);
-                    int lb = this->pX->global2local_row(b);
-                    if (this->pX->in_this_processor(b, j)) X_bj(0, 0, ik * this->pX->get_local_size() + lj * this->pX->get_row_size() + lb) = this->one();
-                    psi::Psi<T> A_aibj(1, 1, this->nk * this->pX->get_local_size()); // k1-first
+                    // X_bj(0, 0, lj * px.get_row_size() + lb) = this->one();
+                    int lj = px.global2local_col(j);
+                    int lb = px.global2local_row(b);
+                    if (px.in_this_processor(b, j)) X_bj(0, 0, ik * px.get_local_size() + lj * px.get_row_size() + lb) = this->one();
+                    psi::Psi<T> A_aibj(1, 1, this->nk * px.get_local_size()); // k1-first
                     A_aibj.zero_out();
 
                     hamilt::Operator<T>* node(this->ops);
@@ -32,9 +35,9 @@ namespace LR
                     A_aibj.fix_kb(0, 0);
 #ifdef __MPI
                     for (int ik_ai = 0;ik_ai < this->nk;++ik_ai)
-                        LR_Util::gather_2d_to_full(*this->pX, &A_aibj.get_pointer()[ik_ai * this->pX->get_local_size()],
+                        LR_Util::gather_2d_to_full(px, &A_aibj.get_pointer()[ik_ai * px.get_local_size()],
                             Amat_full.data() + kbj * this->nk * npairs /*col, bj*/ + ik_ai * npairs/*row, ai*/,
-                            false, this->nvirt, this->nocc);
+                            false, nv, no);
 #endif
                 }
         // output Amat
