a0ed4997deb97b7626751506892c72b419eeaae4
maki49 <1579492865@qq.com>
2025-10-27 21:37:35 +0800

    fix an wrong index in cal_DM_onebase(CXC)

--- source/module_lr/operator_casida/operator_lr_exx.cpp
+++ source/module_lr/operator_casida/operator_lr_exx.cpp
@@ -41,7 +41,7 @@ namespace LR
         {
             // term1: Co -> CvX, i.e. [CvX] Cv
             DMBand<double> dm_band1(ucell, pmat, this->kv.kvec_c, this->BvK_cells, this->cvx_full, this->psi_ks_full);
-            dm_band1.eval(io, iv, ik);
+            dm_band1.eval(io, nocc + iv, ik);
             // term2: Cv -> CoX^T, i.e. Co [CoX^T]
             DMBand<double> dm_band2(ucell, pmat, this->kv.kvec_c, this->BvK_cells, this->psi_ks_full, this->coxt_full);
             dm_band2.eval(io, iv, ik);
